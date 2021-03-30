d3092eb0d9a25cdf026f8de323d4b50a412c2a3f
Damon Garcia Garcia <damon99@hotmail.es>
2021-03-30 11:54:48 +0100

    Ficheros sin utilizar eliminados

--- docker_files/rails_off.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /vagrant/proyecto-rails-restaurante/
-sudo kill $(cat /vagrant/proyecto-rails-restaurante/tmp/pids/server.pid)
-docker-compose down
--- docker_files/rails_on.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /vagrant/proyecto-rails-restaurante/
-docker-compose up -d
-bundle exec rails db:migrate:reset
-bundle exec rails s -d -p 3000 -b '0.0.0.0'
